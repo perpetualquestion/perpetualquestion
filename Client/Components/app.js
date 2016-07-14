488336f1e4fca640bd762757d4d4ec5efb3f4e19
Hongchao Yu <yuhongchao@gmail.com>
2016-07-14 16:07:26 -0700

    Updated app.js

--- Client/Components/app.js
+++ Client/Components/app.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Bootstrap from "./vender/bootstrap-without-jquery";
+import Bootstrap from "";
 import { Router, Route, IndexRoute, hashHistory } from "react-router";
 import Layout from "./pages/Layout";
 
@@ -8,7 +8,8 @@ const app = document.getElementById('app');
 
 ReactDOM.render(
 	<Router history={hashHistory}>
-
-	</Router>
+		<Route path="/" component={Layout}>
+		</Route>
+	</Router>,
 
 app);
