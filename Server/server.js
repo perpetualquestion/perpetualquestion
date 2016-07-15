04aaf6ca120363ea3016ecb6a565b94a589c4cbd
Yuyi <liyuyi828@gmail.com>
2016-07-15 15:03:48 -0700

    Setting up router on server.js file

--- Server/server.js
+++ Server/server.js
@@ -1,11 +1,15 @@
 var express = require('express');
-var router = express.Router();
+var router = require('./router.js');
 
 var app = express(); 
 
-//We may need middleware for express
+//We may need middleware for express, such as body.parser
 
 
+// Setting up router for request 
+app.use('/', router)
+
+// Serving the 'client' folder
 app.use(express.static(__dirname + '/../Client'));
 console.log(__dirname);
 
