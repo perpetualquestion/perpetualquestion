d43e5341f29a5d7ac73484dcc8a255e086c3268c
Matt Aftalion <aftalion.matt@gmail.com>
2016-07-18 15:35:47 -0700

    Fixed basic server

--- Server/server.js
+++ Server/server.js
@@ -3,7 +3,6 @@ var router = require('./router.js');
 
 var app = express(); 
 
-<<<<<<< HEAD
 //We may need middleware for express, such as body.parser
 
 app.use(express.static(__dirname + '/../Client'));
@@ -13,14 +12,12 @@ app.use('/', router)
 
 // Serving the 'client' folder
 console.log(__dirname);
-=======
 //We may need middleware for express
 app.use(express.static(__dirname + '/../Client'));
 
 app.get('/', function(req, res){
   res.render('index')
 })
->>>>>>> 4bab2bd75e9a3dae5d5fde788dc4dc3169a515fa
 
 
 //We may need to set up routing base on our need
