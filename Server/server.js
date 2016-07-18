4bab2bd75e9a3dae5d5fde788dc4dc3169a515fa
Matthew Aftalion <aftalion.matt@gmail.com>
2016-07-18 15:17:20 -0700

    Update server.js

--- Server/server.js
+++ Server/server.js
@@ -3,10 +3,11 @@ var express = require('express');
 var app = express(); 
 
 //We may need middleware for express
-
-
 app.use(express.static(__dirname + '/../Client'));
-console.log(__dirname);
+
+app.get('/', function(req, res){
+  res.render('index')
+})
 
 
 //We may need to set up routing base on our need
