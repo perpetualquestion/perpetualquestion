0d0172b855e1b00541e81776e9454dc841e42d36
Yuyi <liyuyi828@gmail.com>
2016-07-20 16:29:00 -0700

    complete sign in and sign up, with server helper function

---
+++ Client/Components/pages/SignIn.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import Helper from "../../Helper/Helper"
+
+export default class SignIn extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      userName: '', 
+      password: ''
+    }
+  }
+
+  handleUserNameChange(e) {
+    this.setState({userName: e.target.value});
+  }
+
+  handlePasswordChange(e) {
+    this.setState({password: e.target.value});  
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    var requestObj = {
+      userName: this.state.userName.trim(),
+      password: this.state.password.trim()
+    }
+    Helper.signin(requestObj);
+  }
+
+  render() {
+    return (    
+      <div>
+        <h2>Sign In</h2>
+        <form method="post" onSubmit={this.handleSubmit}>
+          <p>User Name:</p>
+          <input type="text" name="userName" value={this.state.userName} onChange={this.handleUserNameChange} required />
+          <p>Password:</p>
+          <input type="text" name="password" value={this.state.password} onChange={this.handlePasswordChange}required />
+          <br />
+          <input type="submit" value="Sign In" />
+        </form>
+      </div>
+      );
+  }
+
+}
