849ea25ade4c03fb1f7f6147d19e24ca748e1038
Hien Tran <aznketchup@yahoo.com>
2016-07-19 19:28:14 -0700

    Resolve merging conflict while rebasing.

--- Client/Components/pages/SearchListEntry.jsx
+++ Client/Components/pages/SearchListEntry.jsx
@@ -1,32 +1,14 @@
 var SearchListEntry = (props) => (
-<<<<<<< HEAD:Client/Components/pages/SearchListEntry.jsx
-
   //Displays full name
   <div>
-  <div>
-    {props.search.firstname + props.search.lastname}  
-  </div>
- //Displays subject
-=======
->>>>>>> Change made to package.json so that npm install and npm start work properly:Client/Components/Search/SearchListEntry.jsx
-  <div>
-  //Displays full name
     <div>
       {props.search.firstname + props.search.lastname}  
     </div>
-   //Displays subject
+    //Display Subject
     <div>
       {props.search.subject}  
     </div>
   </div>
-<<<<<<< HEAD:Client/Components/pages/SearchListEntry.jsx
-  </div>
-
-
 )
 
-=======
-)
-
->>>>>>> Change made to package.json so that npm install and npm start work properly:Client/Components/Search/SearchListEntry.jsx
 export default SearchListEntry;
