33a8acc869d7001071acccc0d4c087f662ee8b38
Hongchao Yu <yuhongchao@gmail.com>
2016-07-20 17:49:30 -0700

    Resovled conflict btw branch and upstream master

--- Client/Components/pages/SearchBar.jsx
+++ Client/Components/pages/SearchBar.jsx
@@ -9,20 +9,23 @@ export default class Search extends React.Component {
     };
   }
 
+  handleSearch(e) {
+    e.preventDefault();
+    console.log(this.refs.text.value.trim());
+  }
+
   handleInputChange() {
-    console.log('This got changed');
+    console.log("Changing the search text...");
   }
 
   render() {
     return (
       <div>
-        <h3>Search</h3>
-        <form>
-          <input
-            type="text" name="lessons"
-            value={this.state.value} />
+        <form method="GET" onSubmit={this.handleSearch.bind(this)}>
+          <h4>Search for skill you want to learn</h4>
+          <input type="text" ref="text" onChange={this.handleInputChange.bind(this)} className="form-control" />
 
-           <input type="submit" value="Search" onClick={this.handleInputChange.bind(this)} />
+          <input type="submit" value="Search" />
         </form>
       </div>
 
