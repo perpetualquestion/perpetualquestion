9ff22ec3d107857fc37c841759acd643f588f39a
Hien Tran <aznketchup@yahoo.com>
2016-07-20 17:59:29 -0700

    Change Navigation Bar
    -Change from vertical navigation bar to horizontal navigation bar
    -Remove home button. Title now take user to home page.

--- Client/Components/pages/layout.jsx
+++ Client/Components/pages/layout.jsx
@@ -9,19 +9,33 @@ export default class Layout extends React.Component {
 	render() {
 		return (
 			<div>
-				<div className="col-md-2 sidebar">
-					<ul className="nav nav-sidebar">
-						<li><button onClick={this.navigate.bind(this)}>Home</button></li>
-						<li><Link to="SearchBar">Search</Link></li>
-						<li><Link to="newfeeds">Newfeeds</Link></li>
-						<li><Link to="profile">Profile</Link></li>
-					</ul>
-				</div>
-				<div className="col-md-10">
+				<nav className="navbar navbar-inverse">
+					<div className="container-fluid">
+						<div className="navbar-header">
+							<a href="/" className="navbar-brand">Perpetual Help Desk</a>
+						</div>
+						<div>
+							<ul className="nav navbar-nav">
+								<li className="active"><Link to="SearchBar">Search</Link></li>
+								<li><Link to="newfeeds">Newfeeds</Link></li>
+								<li><Link to="profile">Profile</Link></li>
+							</ul>
+						</div>
+					</div>
+				</nav>
+				<div>
 					{this.props.children}
 				</div>
 			</div>
 		);
 	}
-
 }
+//Side Navigation bar
+				// <div className="col-md-2 sidebar">
+				// 	<ul className="nav nav-sidebar">
+				// 		<li><button onClick={this.navigate.bind(this)}>Home</button></li>
+				// 		<li><Link to="SearchBar">Search</Link></li>
+				// 		<li><Link to="newfeeds">Newfeeds</Link></li>
+				// 		<li><Link to="profile">Profile</Link></li>
+				// 	</ul>
+				// </div>
