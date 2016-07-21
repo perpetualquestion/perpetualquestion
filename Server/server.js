ef08d145815fbcc32528ec775e1a4006c4a6c734
Matt Aftalion <aftalion.matt@gmail.com>
2016-07-20 20:22:49 -0700

    Set up passport dependencies

--- Server/server.js
+++ Server/server.js
@@ -1,15 +1,14 @@
 var express = require('express');
 var app = express(); 
 var router = require('./router.js');
-
 var cookieParser = require('cookie-parser');
 var session = require('express-session');
 var morgan = require('morgan');
 var bodyParser = require('body-parser');
-
+var passport = require('passport');
+var flash = require('connect-flash');
 
 app.use(express.static(__dirname + '/../Client'));
-
 app.use(morgan('dev'));
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({extended: true}));
@@ -19,10 +18,11 @@ app.use(session({
   resave: true
 }));
 
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
 
 app.use('/', router);
-
-
 
 var port = 3000;
 
